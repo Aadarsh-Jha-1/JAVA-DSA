c2199895202c77c270a7b407b740a37366eaf34e
AADARSH JHA <145737043+Aadarsh-Jha-1@users.noreply.github.com>
2024-10-13 04:25:07 +0000

    "Added Prime_Numbers.java"

---
+++ 01_Basics/Prime_Numbers.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Prime_Numbers {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Number Of Inputs You Want To Give");
+
+        int numOfInputs= sc.nextInt();
+        int[] numbers = new int[numOfInputs];
+
+        int count =0;
+
+        System.out.println("Enter " + numOfInputs + " integers:");
+
+        for (int i=0;i<numOfInputs;i++){
+            numbers[i]=sc.nextInt();
+        }
+
+        for(int j=0;j<=numbers.length;j++){
+            for(int k=1; k<=numbers[j]/2;k++){
+                if(numbers[j]%k == 0 ){
+                    count++;
+                }
+                
+            }
+            if(count>1){System.out.println(numbers[j]+"is not prime");}
+                else{System.out.println(numbers[j]+" is prime");}
+        }
+        sc.close();
+
+    }
+
+    
+}
