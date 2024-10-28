19de3ff79901c4a62f1a0be01a2ca3a52740529c
AADARSH JHA <145737043+Aadarsh-Jha-1@users.noreply.github.com>
2024-10-28 08:58:05 +0000

    Added Benjamin_Bulbs.java

---
+++ 01_Basics/Benjamin_Bulbs.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * Benjamin_Bulbs
+ */
+public class Benjamin_Bulbs {
+
+    
+    public static void main(String[] args) {
+        
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+          
+        for(int i =1; i*i<=n;i++){
+
+            System.out.println(i*i);
+
+        }
+
+
+    }
+
+}
