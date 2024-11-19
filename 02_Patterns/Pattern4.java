22cc0c1dd37f4162fb55ca8f6c594f22d2310545
AADARSH JHA <145737043+Aadarsh-Jha-1@users.noreply.github.com>
2024-11-19 09:59:02 +0000

    Pattern3 And Pattern4.java

---
+++ 02_Patterns/Pattern4.java
@@ -0,0 +1,39 @@
+import  java.util.Scanner;
+
+public class Pattern4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter The Number of lines of stars you want to print : ");
+        int n = sc.nextInt();
+
+        int sp = 0;
+        int st = n;
+
+        for(int i = 1 ; i<=n; i++){
+
+            for(int j = 0; j<sp;j++){
+                System.out.print("\t");
+              }
+              
+          for (int j = 1; j<=st; j++){
+
+            System.out.print("*\t");
+
+          }
+
+          
+
+
+
+          
+            sp++;
+            st--;
+            System.out.println();
+            sc.close();
+        }
+
+      
+    }
+}
